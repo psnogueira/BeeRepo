50f53ecbc794ec829f38d39ef96295fdbfee5d1c
Pedro Nogueira <75403040+psnogueira@users.noreply.github.com>
2024-09-03 18:03:08 -0300

    Implementação do Power BI e alteração da UI.

---
+++ Bee/Controllers/ReportsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bee.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // <iframe title="teste1db" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNjVjNjliMWItOGM3Mi00OGVmLThiOTctZTAyNTgwM2MxY2NkIiwidCI6IjExZGJiZmUyLTg5YjgtNDU0OS1iZTEwLWNlYzM2NGU1OTU1MSIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+
+        public IActionResult PowerBIReport()
+        {
+            return View();
+        }
+    }
+}
