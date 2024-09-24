38e23f66a82387a5c179bfeca2baa1451b143da9
Pedro Nogueira <75403040+psnogueira@users.noreply.github.com>
2024-09-24 12:30:58 -0300

    Upload e Visualização de arquivos .xls e .xlsx de Empresas. Melhoria na UI de Importação.

--- Bee/Controllers/CompaniesController.cs
+++ Bee/Controllers/CompaniesController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Bee.Data;
 using Bee.Models;
+using System.Text;
+using ExcelDataReader;
+using System.Data;
+using System.Diagnostics;
 
 namespace Bee.Controllers
 {
@@ -34,6 +38,91 @@ namespace Bee.Controllers
             }
 
             return View(await company.ToListAsync());
+        }
+
+        public IActionResult Import()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Import(IFormFile upload)
+        {
+            if (ModelState.IsValid)
+            {
+                if(upload != null && upload.Length > 0)
+                {
+                    Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                    Stream stream = upload.OpenReadStream();
+                    IExcelDataReader reader = null;
+                    if (upload.FileName.EndsWith(".xls"))
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                    }
+                    else if (upload.FileName.EndsWith(".xlsx"))
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Arquivo", "Este formato de arquivo não é suportado.");
+                        return View();
+                    }
+                    DataTable dt = new DataTable();
+                    DataRow row;
+                    DataTable dt_ = new DataTable();
+                    try
+                    {
+                        dt_ = reader.AsDataSet().Tables[0];
+                        for (int i = 0; i < dt_.Columns.Count; i++)
+                        {
+                            dt.Columns.Add(dt_.Rows[0][i].ToString());
+                        }
+                        for (int row_ = 1; row_ < dt_.Rows.Count; row_++)
+                        {
+                            row = dt.NewRow();
+                            for (int col = 0; col < dt_.Columns.Count; col++)
+                            {
+                                row[col] = dt_.Rows[row_][col].ToString();
+                            }
+                            dt.Rows.Add(row);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        ModelState.AddModelError("Arquivo", "Erro ao ler o arquivo.");
+                        return View();
+                    }
+                    reader.Close();
+                    reader.Dispose();
+
+                    Debug.WriteLine("Debug teste arquivo (columns):");
+                    // Print all the columns of the table and keep track of the index of the column
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        Debug.WriteLine(i);
+                        Debug.WriteLine(dt.Columns[i].ColumnName);
+                    }
+
+                    Debug.WriteLine("Debug teste arquivo (rows):");
+                    // Print the first 5 rows of the table. If there is no row, it will not print anything.
+                    for (int i = 0; i < dt.Rows.Count && i < 5; i++)
+                    {
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                        {
+                            Debug.WriteLine(dt.Rows[i][j].ToString());
+                        }
+                    }
+
+                    return View(dt);
+                }
+                else
+                {
+                    ModelState.AddModelError("Arquivo", "Por favor, envie um arquivo.");
+                }
+            }
+
+            return View();
         }
 
         // GET: Companies/Details/5
